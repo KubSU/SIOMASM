52ee3beaeaa4f4a404d72d0d079328cf3ca904f0
Rashid Khuramshin <hrs.rashid@gmail.com>
2014-05-31 21:51:29 +0400

    changed test: outint and array
    if just `arr dd 10 dup(1)` then `outint` prints not only `1`
    initialize array with `mov arr[ebx], x`

--- Tests/MainTest.asm
+++ Tests/MainTest.asm
@@ -5,6 +5,9 @@ include /masm32/include/io.asm
 
 lpstrMessage db "Hello, World", 13,10,0
 lpstrMsgInput db "Type number> ",0
+msgArr	db 	"====Array===",0
+num	dd	5
+arr	dd	10 dup(?)
 
 .code
 
@@ -16,22 +19,20 @@ outstr	offset lpstrMessage
 sub	eax, 5
 outint	eax, 3
 newline
+outint	num
+newline
+
+outstr	offset 	msgArr
+newline
 
 mov	ebx, 0
-mov	eax, 1
-
-.while eax
-	outint	eax
-	outch ';'
-	outint ebx
+mov	eax, 2
+.while ebx < 10
+	mov	arr[ebx], eax
+	outint	arr[ebx]
 	newline
-	
 	inc	ebx
-	mov	eax, ebx
-	mov	edx, 0
-	mov 	ecx, 3
-	div	ecx
-	mov 	eax, edx
+	mul	ebx
 .endw
 
 
