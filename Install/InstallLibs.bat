33bab64351711d07ca400912dc66f4e807f08aa3
mpsnp <kir_george@mail.ru>
2014-06-23 20:29:15 +0400

    Absolute path to install dir

--- Install/InstallLibs.bat
+++ Install/InstallLibs.bat
@@ -1,4 +1,4 @@
-cp -f ..\io.asm \masm32\include\io.asm
-cp -f ..\lib\iolib.lib \masm32\lib\iolib.lib
+cp -f ..\io.asm C:\masm32\include\io.asm
+cp -f ..\lib\iolib.lib C:\masm32\lib\iolib.lib
 echo "Library installed. Press Enter."
 pause
